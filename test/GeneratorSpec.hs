51af89b18cb91378064eb30100ec666e82fbdded
Thomas Letan <contact@thomasletan.fr>
2016-04-10 19:15:52 +0200

    Add a test to avoid regression

--- test/GeneratorSpec.hs
+++ test/GeneratorSpec.hs
@@ -16,10 +16,16 @@ import qualified Text.Ogmarkup.Private.Ast        as Ast
 import qualified Text.Ogmarkup.Private.Generator  as Gen
 
 spec :: Spec
-spec = describe "format" $ do
-          it "should deal with raw input" $
-            Gen.runGenerator (Gen.format sentence) testConf
-              `shouldBe` "Bonjour toi."
+spec = do describe "format" $ do
+            it "should deal with raw input" $
+              Gen.runGenerator (Gen.format sentence) testConf
+                `shouldBe` "Bonjour toi."
+
+          describe "component" $ do
+            it "should deal with thought" $
+              Gen.runGenerator (Gen.component False False (Ast.Thought simpleReply Nothing))
+                               testConf
+                `shouldBe` "[thou-anonymous][reply]Bonjour toi.[/reply][/thou-anonymous]"
 
 testConf :: GenConf Text
 testConf = GenConf frenchTypo
@@ -48,3 +54,6 @@ sentence :: Ast.Format Text
 sentence = Ast.Raw [ Ast.Word "Bonjour"
                    , Ast.Word "toi"
                    , Ast.Punctuation Ast.Point ]
+
+simpleReply :: Ast.Reply Text
+simpleReply = Ast.Simple [ sentence ]
