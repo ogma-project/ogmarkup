888a86dc139da0a3f81f2fded66354ceec5cb4c1
Iky <hi@ikyushii.eu>
2016-04-15 10:27:35 +0200

    GenConfig becomes a typeclass

--- src/Text/Ogmarkup/Private/Config.hs
+++ src/Text/Ogmarkup/Private/Config.hs
@@ -1,28 +1,77 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings      #-}
+{-# LANGUAGE MultiParamTypeClasses  #-}
+{-# LANGUAGE FunctionalDependencies #-}
 
 module Text.Ogmarkup.Private.Config where
 
 import           Data.Monoid
+import           Data.String
 import           Text.Ogmarkup.Private.Typography
 
 type Template a = a -> a
 
--- | A data type to carry out the generation configuration. In
---   particular, it works well to define a Typography and some
---   marker such as HTML tags.
-data GenConf a = GenConf { -- | The Typography to use for the generation
-                         typography         :: Typography a,
-                         documentTemplate   :: Template a,
-                         errorTemplate      :: Template a,
-                         storyTemplate      :: Template a,
-                         asideTemplate      :: Maybe a -> Template a,
-                         paragraphTemplate  :: Template a,
-                         tellerTemplate     :: Template a,
-                         dialogueTemplate   :: a -> Template a,
-                         thoughtTemplate    :: a -> Template a,
-                         replyTemplate      :: Template a,
-                         betweenDialogue    :: a,
-                         emphTemplate       :: Template a,
-                         strongEmphTemplate :: Template a,
-                         authorNormalize    :: Maybe a -> a,
-                         printSpace         :: Space -> a }
+class (IsString o, Monoid o) => GenConf c o | c -> o where
+  typography :: c
+             -> Typography o
+  typography _ = englishTypo
+
+  documentTemplate :: c
+                   -> Template o
+  documentTemplate _ = id
+
+  errorTemplate :: c
+                -> Template o
+  errorTemplate _ = id
+
+  storyTemplate :: c
+                -> Template o
+  storyTemplate _ = id
+
+  asideTemplate :: c
+                -> Maybe o
+                -> Template o
+  asideTemplate _ _ = id
+
+  paragraphTemplate :: c
+                    -> Template o
+  paragraphTemplate _ = id
+
+  tellerTemplate :: c
+                 -> Template o
+  tellerTemplate _ = id
+
+  dialogueTemplate :: c
+                   -> o
+                   -> Template o
+  dialogueTemplate _ _ = id
+
+  thoughtTemplate :: c
+                  -> o
+                  -> Template o
+  thoughtTemplate _ _ = id
+
+  replyTemplate :: c
+                -> Template o
+  replyTemplate _ = id
+
+  betweenDialogue :: c
+                  -> o
+  betweenDialogue _ = mempty
+
+  emphTemplate :: c
+               -> Template o
+  emphTemplate _ = id
+
+  strongEmphTemplate :: c
+                     -> Template o
+  strongEmphTemplate _ = id
+
+  authorNormalize :: c
+                  -> Maybe o
+                  -> o
+  authorNormalize _ (Just auth) = auth
+  authorNormalize _ Nothing = mempty
+
+  printSpace :: c
+             -> Space
+             -> o
